ba7d7d405dc6ba42e00e35a206c7eb6cc46dce3b
Marina <marinaakter878@gmail.com>
2024-09-23 16:50:43 +0600

    Refactor HTML and JavaScript code for Payoo homepage and login page

--- js/cashout.js
+++ js/cashout.js
@@ -2,10 +2,30 @@ document.getElementById('btn-cash-out').addEventListener('click', function(event
     event.preventDefault()
     const cashOut = getInputFieldById('input-cash-out')
     const pinNumber = getInputFieldById('input-cash-out-pin')
+    if(isNaN(cashOut)){
+        alert("Failed to Cash Out")
+        return
+    }
     if(pinNumber === 123){
         const balance = getTextFieldById('account-balance')
+        if(cashOut > balance){
+            alert("you do not have enough money to cash out")
+            return
+        }
         updatedBalance = balance - cashOut
         document.getElementById('account-balance').innerText = updatedBalance
+
+        //transaction history
+        const div = document.createElement('div')
+        div.classList.add('text-red-500')
+        div.innerHTML = `
+            <div class="flex items-center space-x-3 bg-red-600 text-white rounded-lg p-3 m-auto">
+            <h6 class="text-lg font-bold">Cash Out</h6>
+            <p>${cashOut} withdraw. New Balance ${updatedBalance} </p>
+            </div>
+        `
+        document.getElementById('transaction-container').appendChild(div)
+
         
     }
     else{
--- js/home.js
+++ js/home.js
@@ -1,21 +1,35 @@
 //add money to the account
-
 //step 1:
-document.getElementById("btn-add-money").addEventListener("click", function (event) {
+document
+  .getElementById("btn-add-money").addEventListener("click", function (event) {
     event.preventDefault();
 
     //step 2:
-    const addMoneyInput = getInputFieldById('input-add-money')
-    const pinNumberInput = getInputFieldById('input-pin-number')
-    
-    
+    const addMoneyInput = getInputFieldById("input-add-money");
+    const pinNumberInput = getInputFieldById("input-pin-number");
+    if (isNaN(addMoneyInput)) {
+      alert("Failed to Add Money");
+      return;
+    }
+
     //step 3:
     if (pinNumberInput === 123) {
       //step 4:
-      const balance = getTextFieldById('account-balance') 
+      const balance = getTextFieldById("account-balance");
       console.log(balance);
-      updatedBalance = balance + addMoneyInput
-      document.getElementById('account-balance').innerText = updatedBalance
+      updatedBalance = balance + addMoneyInput;
+      document.getElementById("account-balance").innerText = updatedBalance;
+
+      const div = document.createElement('div')
+        div.classList.add('text-green-500')
+        div.innerHTML = `
+            <div class="flex items-center space-x-3 bg-green-600 text-white rounded-lg p-3 m-auto">
+            <h6 class="text-lg font-bold">Add Money</h6>
+            <p>${addMoneyInput} withdraw. New Balance ${updatedBalance} </p>
+            </div>
+        `
+        document.getElementById('transaction-container').appendChild(div)
+
 
     } else {
       alert("Failed to Add Money! Please try again.");
